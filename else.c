77bed717920905b2fe7c44fae7f342951613ce6d
Anik Deb <anikdeb3333@gmail.com>
2026-09-19 00:03:15 +0600

    Add files via upload

---
+++ else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int time =20;
+    bool isday=time<18;
+    if(isday){
+        printf("Good day");
+    
+    }
+    else{
+        printf("Good evening");
+	
+	}
+}
+
